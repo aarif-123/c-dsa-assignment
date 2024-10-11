efd8c02fc1942d7885089ea37fde0b96742e15a5
Mohammed <mohdaarif92422@gmail.com>
2024-10-11 20:57:58 +0530

    solved day 32

---
+++ p32.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+class Queue {
+private:
+    Node* front;
+    Node* rear;
+
+public:
+    Queue() {
+        front = rear = nullptr;
+    }
+
+    void enqueue(int val) {
+        Node* temp = new Node(val);
+        if (rear == nullptr) {
+            front = rear = temp;
+            return;
+        }
+        rear->next = temp;
+        rear = temp;
+    }
+
+    void dequeue() {
+        if (front == nullptr) {
+            cout << "Queue is empty" << endl;
+            return;
+        }
+        Node* temp = front;
+        front = front->next;
+        if (front == nullptr)
+            rear = nullptr;
+        delete temp;
+    }
+
+    void display() {
+        if (front == nullptr) {
+            cout << "Queue is empty" << endl;
+            return;
+        }
+        Node* temp = front;
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    Queue q;
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.display();
+    q.dequeue();
+    q.display();
+    return 0;
+}
