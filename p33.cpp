baa19b9b4773143213456979bc39532293a15f15
Mohammed <mohdaarif92422@gmail.com>
2024-10-11 21:03:47 +0530

    added soltuion of day 33

---
+++ p33.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <unordered_map>
+#include <queue>
+#include <string>
+using namespace std;
+
+string frequencySort(string s) {
+    unordered_map<char, int> freq;
+    
+    for (char c : s) {
+        freq[c]++;
+    }
+    
+    priority_queue<pair<int, char>> pq;
+    
+    for (auto it : freq) {
+        pq.push({it.second, it.first});
+    }
+    
+    string result = "";
+    
+    while (!pq.empty()) {
+        auto [count, ch] = pq.top();
+        pq.pop();
+        result.append(count, ch);
+    }
+    
+    return result;
+}
+
+int main() {
+    string s;
+    cout << "Enter a string: ";
+    cin >> s;
+    
+    string sortedString = frequencySort(s);
+    cout << "Sorted string based on frequency: " << sortedString << endl;
+    
+    return 0;
+}
