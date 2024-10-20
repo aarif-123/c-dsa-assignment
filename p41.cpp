f3671e91f0acb9123d31e1ce9edb06a0e95baad1
Mohammed <mohdaarif92422@gmail.com>
2024-10-20 19:04:49 +0530

    d

--- p41.cpp
+++ p41.cpp
@@ -1,3 +1,28 @@
+// Ques 1. Write a recursive function to calculate the factorial of a given number n.
+
+// Example:
+// Input: n = 5
+// Output: 120
+// Explanation: 5! = 5 * 4 * 3 * 2 * 1 = 120.
+
+// Ques 2. Write a recursive function to return the nth Fibonacci number. The Fibonacci sequence is defined as:
+
+// F(0) = 0, F(1) = 1
+// F(n) = F(n-1) + F(n-2) for n > 1
+// Example:
+// Input: n = 6
+// Output: 8
+// Explanation: Fibonacci series = 0, 1, 1, 2, 3, 5, 8,...
+
+// Ques 3. Write a recursive function that takes a positive integer and returns the sum of its digits.
+
+// Example:
+// Input: n = 1234
+// Output: 10
+// Explanation: 1 + 2 + 3 + 4 = 10
+
+
+
 #include <iostream>
 using namespace std;
 
