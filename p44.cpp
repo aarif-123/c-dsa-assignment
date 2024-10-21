82498a2cd5b864d76dab317733ac454cf0fdbfe6
Mohammed <mohdaarif92422@gmail.com>
2024-10-21 19:11:43 +0530

    dfd

---
+++ p44.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+void inorderTraversal(Node* root) {
+    if (root == nullptr) {
+        return;
+    }
+
+    inorderTraversal(root->left);
+    cout << root->data << " ";
+    inorderTraversal(root->right);
+}
+
+int main() {
+ 
+    Node* root = new Node(1);
+    
+    root->left = new Node(2);
+
+    root->right = new Node(3);
+
+    root->left->left = new Node(4);
+    
+    root->left->right = new Node(5);
+
+    root->right->right = new Node(6);
+
+    
+    cout << "Inorder Traversal of the binary tree: ";
+    inorderTraversal(root);
+    cout << endl;
+
+    return 0;
+}
